f69d1f10673bf591813e24c620a6f53dafab254d
Jason Pottorff <77120503+Jazzyspoon@users.noreply.github.com>
2021-08-19 21:56:59 -0600

    updated folder structure

--- src/clocks/newyearsday/newyearsday.js
+++ src/clocks/newyearsday/newyearsday.js
@@ -1,7 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import Countdown from "react-countdown";
-import "./timer.css";
+import "./NYtimer.css";
 import "../App.css";
 
 // Random component
